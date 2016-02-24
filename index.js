bf73f1737c43be3c4994a52f1518d7a95b5daa63
Wolfy <tyr@pettingzoo.co>
2016-02-24 00:14:49 -0800

    connect to redis

--- index.js
+++ index.js
@@ -10,3 +10,13 @@ var amqp   = require('amqplib/callback_api'),
 log4js.configure('', config.l4j);
 var log    = log4js.getLogger();
 var rdsLog = log4js.getLogger('redis');
+
+// init Redis
+var redisClient = redis.createClient(config.redis.options);
+
+redisClient.on('connect',function(){
+  rdsLog.info('connected to %s', config.redis.options.host);
+});
+redisClient.on('error',function(err){
+  rdsLog.error(err);
+});
