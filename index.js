cffe355bb03b42d9ea8863c0720c8213248d57e2
Wolfy <tyr@pettingzoo.co>
2016-02-23 23:59:02 -0800

    init logging

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ var config = require('./config');
 var amqp   = require('amqplib/callback_api'),
     log4js = require('log4js'),
     redis  = require('redis');
+
+// Init Logging
+log4js.configure('', config.l4j);
+var log    = log4js.getLogger();
+var rdsLog = log4js.getLogger('redis');
